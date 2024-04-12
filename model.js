5505a646f0d3c7df7f142ac17bcd39d22993db7c
Manan Agarwal <109158062+The-unpredictable@users.noreply.github.com>
2024-04-12 11:32:53 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,21 @@
+{
+    "0": {
+        "barcode_value": 0,
+        "model_name": "base",
+        "model_url": "#",
+        "position": {"x": 0, "y": 0, "z": 0 },
+        "rotation": { "x": -90, "y": 0, "z": 0},
+        "color": "#795548" ,
+        "width": 19,
+        "height": 15
+    },
+    "1": {
+        "barcode_value": 1,
+        "model_name": "road"
+        "model_url": "https://raw.githubusercontent.com/whitehatjr/three-d-models/main/model_gltf_6YmjcoM2ebx_6dQpF5pzTPc/model.gltf",
+        "position": { "x": 0, "y": -90, "z": 0},
+        "rotation": { "x" : 0, "y": -90 "z": 0},
+        "scale": { "x": 3, "y": 3, "z": 3}
+
+    }
+}
